813128297b7e34372b5a0f560de6acf27e286a8f
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-08-09 17:24:48 +0700

    fix: fix prefix active page

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -6,19 +6,19 @@
         <span class="group-label">Navigation</span>
     </div>
     <div class="group-menus">
-        <a href="/overview" class="group-menu {{ request()->is('overview') ? 'active' : '' }}">
+        <a href="/overview" class="group-menu {{ request()->is('overview*') ? 'active' : '' }}">
             <img src="{{ asset('assets/images/overview.svg') }}" width="21" alt="group-menu-icon" class="group-menu-icon">
             <span class="group-menu-text">Overview</span>
         </a>
-        <a href="/users" class="group-menu {{ request()->is('users') ? 'active' : '' }}">
+        <a href="/users" class="group-menu {{ request()->is('users*') ? 'active' : '' }}">
             <img src="{{ asset('assets/images/users.svg') }}" width="21" alt="group-menu-icon" class="group-menu-icon">
             <span class="group-menu-text">Users</span>
         </a>
-        <a href="#" class="group-menu {{ request()->is('presences') ? 'active' : '' }}">
+        <a href="#" class="group-menu {{ request()->is('presences*') ? 'active' : '' }}">
             <img src="{{ asset('assets/images/presences.svg') }}" width="21" alt="group-menu-icon" class="group-menu-icon">
             <span class="group-menu-text">Presences</span>
         </a>
-        <a href="#" class="group-menu {{ request()->is('reports') ? 'active' : '' }}">
+        <a href="#" class="group-menu {{ request()->is('reports*') ? 'active' : '' }}">
             <img src="{{ asset('assets/images/reports.svg') }}" width="21" alt="group-menu-icon" class="group-menu-icon">
             <span class="group-menu-text">Reports</span>
         </a>
