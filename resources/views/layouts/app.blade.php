76a2735e4307765dc4251a50a9886d9931e8fc2b
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-08-09 17:21:31 +0700

    fix: fix assets resource not found

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="./assets/stylesheets/style.css">
+    <link rel="stylesheet" href="{{ asset('assets/stylesheets/style.css') }}">
 </head>
 <body>
     @include('partials.sidebar')
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,25 +1,25 @@
 <nav class="sidebar">
     <div class="logo-wrapper">
-        <img src="./assets/images/logo.png" alt="logo" class="logo" height="40">
+        <img src="{{ asset('assets/images/logo.png') }}" alt="logo" class="logo" height="40">
     </div>
     <div class="group-label-wrapper">
         <span class="group-label">Navigation</span>
     </div>
     <div class="group-menus">
         <a href="/overview" class="group-menu {{ request()->is('overview') ? 'active' : '' }}">
-            <img src="./assets/images/overview.svg" width="21" alt="group-menu-icon" class="group-menu-icon">
+            <img src="{{ asset('assets/images/overview.svg') }}" width="21" alt="group-menu-icon" class="group-menu-icon">
             <span class="group-menu-text">Overview</span>
         </a>
         <a href="/users" class="group-menu {{ request()->is('users') ? 'active' : '' }}">
-            <img src="./assets/images/users.svg" width="21" alt="group-menu-icon" class="group-menu-icon">
+            <img src="{{ asset('assets/images/users.svg') }}" width="21" alt="group-menu-icon" class="group-menu-icon">
             <span class="group-menu-text">Users</span>
         </a>
         <a href="#" class="group-menu {{ request()->is('presences') ? 'active' : '' }}">
-            <img src="./assets/images/presences.svg" width="21" alt="group-menu-icon" class="group-menu-icon">
+            <img src="{{ asset('assets/images/presences.svg') }}" width="21" alt="group-menu-icon" class="group-menu-icon">
             <span class="group-menu-text">Presences</span>
         </a>
         <a href="#" class="group-menu {{ request()->is('reports') ? 'active' : '' }}">
-            <img src="./assets/images/reports.svg" width="21" alt="group-menu-icon" class="group-menu-icon">
+            <img src="{{ asset('assets/images/reports.svg') }}" width="21" alt="group-menu-icon" class="group-menu-icon">
             <span class="group-menu-text">Reports</span>
         </a>
     </div>
